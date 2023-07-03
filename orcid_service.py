9eb9481914a5745a3b80467c0a42adc7ab755b9c
Daniel Souza <DCarts@users.noreply.github.com>
2023-07-02 23:44:05 -0300

    refactor(): carrega o search_token do ORCID apenas se necessário

--- orcid_service.py
+++ orcid_service.py
@@ -9,8 +9,7 @@ if os.path.isfile('secret.json'):
 
     
 orcid_api = orcid.PublicAPI(secrets.get('orcid_client_id'), secrets.get('orcid_client_secret'), sandbox=False)
-orcid_search_token = orcid_api.get_search_token_from_orcid()
-
+orcid_search_token = None
 def load_dict(file_json_path):
     if os.path.isfile(file_json_path):
         with open(file_json_path, 'r') as input_file:
@@ -27,5 +26,7 @@ def load_orcid(orcid, orcid_dict=None, force=False):
     if orcid_dict is None:
         orcid_dict = load_dict(os.path.join('data', 'orcid.json'))
     if orcid not in orcid_dict or force:
+        if orcid_search_token is None:
+            orcid_search_token = orcid_api.get_search_token_from_orcid()
         orcid_dict[orcid] = orcid_api.read_record_public(orcid, 'person', orcid_search_token)
     return orcid_dict
