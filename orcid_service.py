b0d7d1c86de0f347fe545d136b7a25212ff0051a
Daniel Souza <DCarts@users.noreply.github.com>
2023-07-04 00:06:43 -0300

    refactor(): load_orcid com parâmetro método e funcionalidade via cli

--- orcid_service.py
+++ orcid_service.py
@@ -22,11 +22,19 @@ def save_dict(json_dict, file_json_path):
     with open(file_json_path, 'w') as output_file:
         output_file.write(json_str)
 
-def load_orcid(orcid, orcid_dict=None, force=False):
+def load_orcid(orcid, orcid_dict=None, force=False, method='person'):
+    global orcid_search_token
     if orcid_dict is None:
         orcid_dict = load_dict(os.path.join('data', 'orcid.json'))
-    if orcid not in orcid_dict or force:
+    if orcid not in orcid_dict:
+        orcid_dict[orcid] = {}
+    if method not in orcid_dict[orcid] or force:
         if orcid_search_token is None:
             orcid_search_token = orcid_api.get_search_token_from_orcid()
-        orcid_dict[orcid] = orcid_api.read_record_public(orcid, 'person', orcid_search_token)
+        orcid_dict[orcid][method] = orcid_api.read_record_public(orcid, method, orcid_search_token)
+
     return orcid_dict
+
+if __name__ == '__main__':
+    orcid_search_token = orcid_api.get_search_token_from_orcid()
+    print(json.dumps(orcid_api.read_record_public(input('orcid: '), input('metodo: '), orcid_search_token), indent=2))
